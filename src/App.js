6485403bdb37165c882de9b92ab9f2c94eca8d4f
Nguyen Khac Khanh <encacap@gmail.com>
2022-04-13 14:53:47 +0700

    fix: try to fix errors

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export const App = ({
 }) => {
     return (
         <React.Fragment>
-            {userData && (
+            {userData && LinkStarIcons && (
                 <div className="linkstar">
                     <div className="relative w-full h-full">
                         <div className="absolute w-full h-full">
